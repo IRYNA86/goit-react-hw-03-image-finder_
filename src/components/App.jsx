36492847d5ac194523724c5fce903b744c3bfcb5
IRYNA86 <88017665+IRYNA86@users.noreply.github.com>
2022-06-15 22:26:21 +0300

    осталась кнопка Loadmore и модалка

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,31 @@ import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
-import { getImages } from '../service/API.js';
+import axios from 'axios';
+import mapper from '../service/mapper.js'
+
+
+const API_KEY = '25251210-ac1999c1ffdbc1fb6fbdee37e';
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
 
 class App extends Component {
   state = {
     images: [],
+    page: 1,
+    per_page: 12,
     nameImages: '',
     isLoading: false,
     error: null,
   };
 
   async componentDidMount() {
-    const { nameImages } = this.state;
-    this.setState({ isLoading: true });
-
+    const {nameImages, page, per_page} = this.state
+        this.setState({ isLoading: true });
     try {
-      const images = getImages(nameImages);
-      this.setState({ images });
+      const response  = await axios.get(`/?key=${API_KEY}&q=${nameImages}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`);
+      this.setState({images: response.data.hits });
+      console.log(response)
     } catch (error) {
       this.setState({ error });
     } finally {
@@ -26,22 +34,36 @@ class App extends Component {
     }
   }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   const prevName = prevState.nameImages;
-  //   const nameImages = this.state.nameImages;
-  //   if (prevName !== nameImages) {
-  //     console.log('Изменилось имя');
-  //     fetch(getImages(nameImages))
-  //       .then(res => res.json())
-  //       .then(images => this.setState(images.data.hits));
-  //   }
-  // }
-  handleFormSubmit = nameImages => {
-    this.setState({ nameImages });
+  async componentDidUpdate(prevProps, prevState){
+    const {nameImages, page, per_page} = this.state
+    if(prevState.nameImages !== nameImages){
+    try{
+      this.setState({ isLoading: true });
+      const images = await axios.get(`/?key=${API_KEY}&q=${nameImages}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`);
+      console.log(images)
+      const imagesData = mapper(images.data.hits);
+      console.log(imagesData)
+      this.setState(prevState  => {
+        return {
+        images: [...prevState.images, ...imagesData],
+        isLoading: false,
+      }})
+    }
+   catch(error){
+    this.setState({error: true, isLoading: false})
+   }}
+  };
+
+  handleFormSubmit = queryValue => {
+    this.setState({ images: [],
+      isLoading: false,
+      page: 1,
+      nameImages: queryValue, });
   };
 
   render() {
     const { images, isLoading, error, nameImages } = this.state;
+    console.log(images)
     return (
       <div>
         <Searchbar onSubmit={this.handleFormSubmit} />
